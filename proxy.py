5e95e3e7b25f89cfa1fd10a2b145db1296ff4fb8
Andrey Kostakov <bps@dzen.ws>
2014-03-20 23:43:18 +0400

    experimental implementation based on asyncio

--- proxy.py
+++ proxy.py
@@ -1,59 +1,46 @@
-import asyncore
-import socket
+import asyncio
 
 
-class ResponseHandler(asyncore.dispatcher):
-    def __init__(self, sock, data):
-        asyncore.dispatcher.__init__(self)
-        self.sock = sock
-        self.sock.send(data)
+class HttpClient(asyncio.Protocol):
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        self.server_transport.write(data)
 
 
-class HttpClient(asyncore.dispatcher):
-    def __init__(self, sock, host, data):
-        asyncore.dispatcher.__init__(self)
-        self.sock = sock
-        self.data = data
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect((host, 80))
+class ProxyServer(asyncio.Protocol):
 
-    def handle_connect(self):
-        self.send(self.data)
+    @asyncio.coroutine
+    def parse_request(self, data):
+        pos = data.find(b'\r\n\r\n')
+        src_headers = data[:pos]
+        headers = src_headers.split(b'\r\n')
+        method, url, http_version = headers[0].split(b' ')
+        header = dict((x[:x.find(b':')].lower(), x[x.find(b':')+1:].strip()) for x in headers[1:])
+        print(header[b'host'])
+        protocol, client = yield from asyncio.get_event_loop().create_connection(HttpClient, header[b'host'], 80)
+        client.server_transport = self.transport
+        client.transport.write(data)
 
-    def handle_read(self):
-        data = self.recv(8192)
-        if data:
-            ResponseHandler(self.sock, data)
-
-
-class RequestHandler(asyncore.dispatcher):
-    def handle_read(self):
-        data = self.recv(8192)
-        if data:
-            pos = data.find(b'\r\n\r\n')
-            src_headers = data[:pos]
-            headers = src_headers.split(b'\r\n')
-            method, url, http_version = headers[0].split(b' ')
-            header = dict((x[:x.find(b':')].lower(), x[x.find(b':')+1:].strip()) for x in headers[1:])
-            HttpClient(self, header[b'host'], data)
+    def connection_made(self, transport):
+        self.transport = transport
 
 
-class ProxyServer(asyncore.dispatcher):
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((host, port))
-        self.listen(5)
-
-    def handle_accept(self):
-        pair = self.accept()
-        if pair is not None:
-            sock, addr = pair
-            print('Incoming connection from {0}'.format(repr(addr)))
-            RequestHandler(sock)
+    def data_received(self, data):
+        asyncio.Task(self.parse_request(data))
 
 
 if __name__ == '__main__':
-    server = ProxyServer('localhost', 8080)
-    asyncore.loop()
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    coro = loop.create_server(ProxyServer, '127.0.0.1', 8080)
+    server = loop.run_until_complete(coro)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("exit")
+    finally:
+        server.close()
+        loop.close()
