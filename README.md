c0deadbb6f4aa3ca2ff88c81b36f25271e5f4bb9
Andrey Kostakov <bps@dzen.ws>
2014-04-14 22:51:26 +0400

    add setup.py

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Small SOCKS5 proxy server on python3.
 proxy.py 0.0.0.0 1080
 ```
 
+### Requirements
+python 3.4 or python 3.3 with asyncio module installed
+
 ### Roadmap
 * setup.py
 * domain name support for cmd connect
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='MiMiProxy',
+    packages=['MiMiProxy'],
+    version='0.0.1',
+    description='Small SOCKS5 proxy server on python3',
+    author='Andrey Kostakov',
+    author_email='bps@dzen.ws',
+    url='https://github.com/AyumuKasuga/MimiProxy',   # use the URL to the github repo
+    download_url='https://github.com/AyumuKasuga/MimiProxy/releases', # I'll explain this in a second
+    keywords=['proxy', 'socks5', 'python3'],
+    classifiers=[],
+)
