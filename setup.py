26635efe2725154a9b1aa6277658413c11d15bd2
Andrey Kostakov <bps@dzen.ws>
2014-04-16 22:19:51 +0400

    fix

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=['MimiProxy'],
     version='0.0.1-alpha',
     description='Small SOCKS5 proxy server on python3',
-    long_description=readme,
+    # long_description=readme,
     author='Andrey Kostakov',
     author_email='bps@dzen.ws',
     url='https://github.com/AyumuKasuga/MimiProxy',
