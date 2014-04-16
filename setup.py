1fcf6485edcfcfb7f0b9e63c556a48870b8b7ea0
Andrey Kostakov <bps@dzen.ws>
2014-04-16 22:14:06 +0400

    some changes in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from distutils.core import setup
+import sys
+
+if sys.version_info[0] >= 3 and sys.version_info[1] >= 3:
+    #ok
+    pass
+else:
+    print("Sorry, this version requires python 3.3 or higher")
+    sys.exit()
+
+readme = open('README.md').read()
+
+requires = ['asyncio',]
 
 setup(
-    name='MiMiProxy',
-    packages=['MiMiProxy'],
-    version='0.0.4',
+    name='MimiProxy',
+    packages=['MimiProxy'],
+    version='0.0.1-alpha',
     description='Small SOCKS5 proxy server on python3',
+    long_description=readme,
     author='Andrey Kostakov',
     author_email='bps@dzen.ws',
     url='https://github.com/AyumuKasuga/MimiProxy',
@@ -12,7 +25,7 @@ setup(
     keywords=['proxy', 'socks5', 'python3'],
     entry_points={
         'console_scripts': [
-            'MiMiProxy = MiMiProxy.proxy:run',
+            'mimiproxy = MimiProxy.proxy:run',
         ],
     },
     classifiers=[
